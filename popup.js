0454f1801a3bb400fbfc42e380c51ad9106a9e3b
MATTHEW NGAI <MTFNGAI@uwaterloo.ca>
2017-02-25 23:58:21 -0500

    edit popup

--- popup.js
+++ popup.js
@@ -1,36 +1,12 @@
-/**
- * Get the current URL.
- *
- * @param {function(string)} callback - called when the URL of the current tab
- *   is found.
- */
 function getCurrentTabUrl(callback) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
-  var queryInfo = {
-    active: true,
-    currentWindow: true
-  };
-
-  chrome.tabs.query(queryInfo, function(tabs) {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
-    // A window can only have one active tab at a time, so the array consists of
-    // exactly one tab.
-    var tab = tabs[0];
-
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
-    var url = tab.url;
-
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
-    console.assert(typeof url == 'string', 'tab.url should be a string');
-
-    callback(url);
+  chrome.tabs.executeScript( {
+    code: "callback(window.getSelection().toString());"
+  });
+  chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+      if (request.method == "getSelection")
+        sendResponse({data: window.getSelection().toString()});
+      else
+        sendResponse({}); // snub them.
   });
 }
 
@@ -81,7 +57,7 @@ function bingTTS(urlLink, text, errorCallback) {
 }
 
 function bingTrans(url, text, resultCallback, errorCallback) {
-  var rn = Math.floor((Math.random() * 10000) + 1);
+  var rn = Math.floor((Math.random() * 100000) + 1);
   var data = "[{id: " + rn + ", text: \"" + text + "\"}]";
   var xhr = new XMLHttpRequest();
   xhr.withCredentials = true;
@@ -105,26 +81,27 @@ function bingTrans(url, text, resultCallback, errorCallback) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  getCurrentTabUrl(function(url) {
-    var oddUrl = "http://cache-a.oddcast.com/c_fs/440044ed609bc14281cbd9b2a466c450.mp3?engine=4&language=10&voice=1&text=";
-    var lang = "yue";
-    var myUrl = "http://www.bing.com/translator/api/language/Speak?locale=" + lang + "&gender=male&media=audio%2Fmp3&text=";
-    var myUrl_t = "http://www.bing.com/translator/api/Translate/TranslateArray?from=-&to=" + lang;
-    var finalText = 'Hello';
-    var translateText = 'Does this work?';
+  getCurrentTabUrl(
+    function(selectedText) {
+      console.log(selectedText);
+      var oddUrl = "http://cache-a.oddcast.com/c_fs/440044ed609bc14281cbd9b2a466c450.mp3?engine=4&language=10&voice=1&text=";
+      var lang = "yue";
+      var myUrl = "http://www.bing.com/translator/api/language/Speak?locale=" + lang + "&gender=male&media=audio%2Fmp3&text=";
+      var myUrl_t = "http://www.bing.com/translator/api/Translate/TranslateArray?from=-&to=" + lang;
+      var finalText = 'Hello';
+      var translateText = 'Does this work?';
 
-
-    // bingTrans(myUrl_t, translateText, 
-    // function(resultCallback) {
-    //   finalText = resultCallback;
-    //   //call function inside result callback
-    //   bingTTS(myUrl, encodeURI(finalText), function(errorMessage) {
-    //     renderStatus('Cannot retrieve speech: ' + errorMessage);
-    //   });
-    // },
-    // function(errorMessage) {
-    //   renderStatus('Cannot translate: ' + errorMessage);
-    // });
+      // bingTrans(myUrl_t, translateText, 
+      // function(resultCallback) {
+      //   finalText = resultCallback;
+      //   //call function inside result callback
+      //   bingTTS(myUrl, encodeURI(finalText), function(errorMessage) {
+      //     renderStatus('Cannot retrieve speech: ' + errorMessage);
+      //   });
+      // },
+      // function(errorMessage) {
+      //   renderStatus('Cannot translate: ' + errorMessage);
+      // });
 
 
   });
